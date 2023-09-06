07e223094611bc97f3b7124e3b30c002d7d5fc27
ayandele <adele.ayansola@yahoo.com>
2023-09-06 11:33:24 -0400

    server.js file updates

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const path = require('path');
+const { ApolloServer } = require('apollo-server-express'); // Import Apollo Server
 const db = require('./config/connection');
-const routes = require('./routes');
+const { typeDefs, resolvers } = require('./graphql'); // Import your GraphQL schema and resolvers
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -14,8 +16,26 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+// Create an instance of Apollo Server
+const server = new ApolloServer({
+typeDefs,
+resolvers,
+  context: ({ req }) => {
+    // Implement authentication logic here 
+    // access req.headers.authorization to get the token and perform user authentication.
+    return {
+      // Add your context data here
+    };
+  },
+});
+
+// Apply Apollo Server as middleware to Express
+server.applyMiddleware({ app });
+
+
+//app.use(routes);
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => 
+    console.log(`🌍 Now listening on localhost:${PORT}`));
 });
